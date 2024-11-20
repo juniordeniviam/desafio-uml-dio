66b1999204c3afc51ccda7c2338ea14f37794040
José Carlos da Silva Júnior <51302974+juniordeniviam@users.noreply.github.com>
2024-11-20 18:03:40 -0300

    Desafio concluído - Adicionando arquivos .java
    Adicionando as classes que foram elaboradas em UML.

---
+++ iPhone.java
@@ -0,0 +1,24 @@
+
+public class iPhone {
+    private ReprodutorMusical reprodutorMusical;
+    private AparelhoTelefonico aparelhoTelefonico;
+    private NavegadorInternet navegadorInternet;
+
+    public iPhone() {
+        this.reprodutorMusical = new ReprodutorMusical();
+        this.aparelhoTelefonico = new AparelhoTelefonico();
+        this.navegadorInternet = new NavegadorInternet();
+    }
+
+    public ReprodutorMusical getReprodutorMusical() {
+        return reprodutorMusical;
+    }
+
+    public AparelhoTelefonico getAparelhoTelefonico() {
+        return aparelhoTelefonico;
+    }
+
+    public NavegadorInternet getNavegadorInternet() {
+        return navegadorInternet;
+    }
+}
